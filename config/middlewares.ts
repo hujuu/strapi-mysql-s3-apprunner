ff347f50a2ce87c98a5a43371ff0dcdd6297406c
hujuu <1961946+hujuu@users.noreply.github.com>
2023-01-09 20:08:25 +0900

    Update strapi::security

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,31 @@
-export default [
+export default({ env }) => [
   'strapi::errors',
-  'strapi::security',
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com/strapi/`,
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com/strapi/`,
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
